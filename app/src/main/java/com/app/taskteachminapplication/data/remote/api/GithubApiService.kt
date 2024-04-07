921f2ed877777779e78a024d773f451c5956ec0a
bhupendrasmt <bhupendrasmt@gmail.com>
2024-04-07 12:44:28 +0530

    Add GitHub API service interface and data classes
    GithubApiService: Interface for GitHub repository search endpoints.
    ApiResponse: Structure for responses from GitHub API.
    RepositoryItem: Data class representing GitHub repositories.

---
+++ app/src/main/java/com/app/taskteachminapplication/data/remote/api/GithubApiService.kt
@@ -0,0 +1,20 @@
+package com.app.taskteachminapplication.data.remote.api
+
+import com.app.taskteachminapplication.data.model.ApiResponse
+import com.app.taskteachminapplication.data.model.RepositoryItem
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface GithubApiService {
+    @Headers(
+        "Accept: application/vnd.github+json",
+        "Authorization: Bearer <Access_Token>"
+    )
+    @GET("search/repositories")
+    suspend fun searchRepositories(
+        @Query("q") query: String,
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
+    ): ApiResponse<List<RepositoryItem>>
+}
